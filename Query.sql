0a1f5f53aeff1b51ff23d33d7cd1d71792df10dd
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-10-21 00:42:16 +0530

    IS NULL

--- Query.sql
+++ Query.sql
@@ -1,6 +1,5 @@
 SELECT * 
 FROM customers
-WHERE first_name REGEXP 'ELKA|AMBUR' 
-WHERE last_name REGEXP 'EY$|ON$'
-WHERE last_name REGEXP '^MY|SE'
-WHERE last_name REGEXP 'B[RU]'
+WHERE phone IS NULL
+
+--we can get the null values using the IS NULL
